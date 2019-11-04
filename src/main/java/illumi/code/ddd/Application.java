332e6dc40f5ea912ea537fcee64e5b9f2737e3b1
Daniel Kraft <daniel.kraft@adesso.de>
2019-11-04 12:09:56 +0100

    Inital commit

---
+++ src/main/java/illumi/code/ddd/Application.java
@@ -0,0 +1,10 @@
+package illumi.code.ddd;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class);
+    }
+}
