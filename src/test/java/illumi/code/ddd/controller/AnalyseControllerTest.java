f6c4439ce5d4af8bae224b197fce5561cbe173f7
DanielKraft <daniel.kraft96@googlemail.com>
2019-11-05 15:42:31 +0100

    Implemented analyze controller test

---
+++ src/test/java/illumi/code/ddd/controller/AnalyseControllerTest.java
@@ -0,0 +1,62 @@
+package illumi.code.ddd.controller;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import javax.inject.Inject;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import illumi.code.ddd.service.AnalyseService;
+import illumi.code.ddd.service.AnalyseServiceImpl;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.client.RxHttpClient;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.test.annotation.MicronautTest;
+import io.micronaut.test.annotation.MockBean;
+
+@MicronautTest
+public class AnalyseControllerTest {
+
+	@Inject
+	AnalyseService analyseService; 
+	
+	@Inject
+    @Client("/")
+    RxHttpClient client;
+	
+	@MockBean(AnalyseServiceImpl.class) 
+    public AnalyseService analyseService() {
+        return mock(AnalyseService.class); 
+    }
+
+	@Test
+    public void testAnalyzeStructure() {
+		
+		JSONArray data = new JSONArray();
+		data.put(new JSONObject()
+				.put("contains", new JSONArray()
+						.put(new JSONObject().put("DDD", "VALUE_OBJECT").put("name", "Visit"))
+						.put(new JSONObject().put("DDD", "REPOSITORY").put("name", "VisitRepository")))
+				.put("DDD", "MODULE")
+				.put("name", "visit"));
+		
+		when(analyseService.analyzeStructure("org.petclinic")).then(invocation -> data);
+		
+		final String expected = "[{\"contains\":["
+									+ "{\"DDD\":\"VALUE_OBJECT\",\"name\":\"Visit\"},"
+									+ "{\"DDD\":\"REPOSITORY\",\"name\":\"VisitRepository\"}],"
+								+ "\"DDD\":\"MODULE\",\"name\":\"visit\"}]";
+		
+		final String result = client.toBlocking().retrieve(HttpRequest.GET("/analyse/org.petclinic"));
+
+    	Assertions.assertEquals(expected, result);
+        
+        verify(analyseService, times(1)).analyzeStructure("org.petclinic");
+	}
+}
