853bde018a8842133cc6e6162ff698823c76ed60
DanielKraft <daniel.kraft96@googlemail.com>
2019-11-19 12:14:27 +0100

    Fix code smell

--- src/main/java/illumi/code/ddd/model/artifacts/Enum.java
+++ src/main/java/illumi/code/ddd/model/artifacts/Enum.java
@@ -1,14 +1,9 @@
 package illumi.code.ddd.model.artifacts;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.neo4j.driver.v1.Driver;
 import org.neo4j.driver.v1.Record;
 
 import illumi.code.ddd.model.DDDFitness;
 import illumi.code.ddd.model.DDDType;
-import illumi.code.ddd.service.JavaArtifactService;
 
 public class Enum extends File {
 
