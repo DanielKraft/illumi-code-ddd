9e2781e59e56025bb375b41c4bb1a8a04761045a
DanielKraft <daniel.kraft96@googlemail.com>
2019-11-26 17:23:59 +0100

    Implemented RefactorServiceTest

---
+++ src/test/java/illumi/code/ddd/service/refactor/impl/RefactorServiceImplTest.java
@@ -0,0 +1,58 @@
+package illumi.code.ddd.service.refactor.impl;
+
+import illumi.code.ddd.model.DDDStructure;
+import illumi.code.ddd.model.DDDType;
+import illumi.code.ddd.model.artifacts.Artifact;
+import illumi.code.ddd.model.artifacts.Class;
+import illumi.code.ddd.model.artifacts.Package;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class RefactorServiceImplTest {
+
+    private RefactorServiceImpl service;
+
+    @BeforeAll
+    void init() {
+        service = new RefactorServiceImpl();
+
+        DDDStructure structure = new DDDStructure();
+        structure.setPath("de.test");
+
+        Package module = new Package("test", "de.test");
+        module.setType(DDDType.MODULE);
+
+        Class root2 = new Class("Root2", "de.Root2");
+        root2.setType(DDDType.AGGREGATE_ROOT);
+        root2.setDomain("domain1");
+        module.addContains(root2);
+        structure.addClass(root2);
+
+        Class root = new Class("Root", "de.test.Root");
+        root.setType(DDDType.AGGREGATE_ROOT);
+        root.setDomain("domain0");
+        module.addContains(root);
+        structure.addClass(root);
+
+        ArrayList<Artifact> data = new ArrayList<>();
+        data.add(module);
+        structure.setStructure(data);
+
+        service.setOldStructure(structure);
+    }
+
+    @Test
+    void testRefactor() {
+        final DDDStructure result = service.refactor();
+
+        assertAll(	() -> assertEquals(12, result.getPackages().size(), "#Package"),
+                    () -> assertEquals(2, result.getClasses().size(), "#Class"));
+    }
+}
