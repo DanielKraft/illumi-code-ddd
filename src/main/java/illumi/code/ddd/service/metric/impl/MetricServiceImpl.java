fd2d2f4e46bcd9b36b1d9b36aab77849277d1c59
DanielKraft <daniel.kraft96@googlemail.com>
2019-12-05 13:33:07 +0100

    Fixed code smells

--- src/main/java/illumi/code/ddd/service/metric/impl/MetricServiceImpl.java
+++ src/main/java/illumi/code/ddd/service/metric/impl/MetricServiceImpl.java
@@ -1,16 +1,10 @@
 package illumi.code.ddd.service.metric.impl;
 
-import java.util.ArrayList;
-
 import javax.inject.Inject;
 
 import illumi.code.ddd.model.DDDStructure;
-import illumi.code.ddd.model.artifacts.Package;
 import illumi.code.ddd.service.metric.MetricService;
 import org.json.JSONObject;
-
-import illumi.code.ddd.model.artifacts.Artifact;
-
 
 public class MetricServiceImpl implements MetricService {
 	    
